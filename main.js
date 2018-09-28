cfc419fa0894be42f1bc4c9cd8a1ba937057e11d
Jeremiah Schmutz <Jeremiah.Schmutz@gmail.com>
2018-09-28 17:10:17 -0400

    added checkboxes and js for an array.

--- main.js
+++ main.js
@@ -4,8 +4,23 @@ $("#submit-btn").on("click", function (event) {
     var locationz = $("#locationz").val().trim();
     var startDate = $("#start-date").val().trim();
     var endDate = $("#end-date").val().trim();
+    var eventParameters = [];
+
+    if ($("#outdoor-check").prop( "checked" )){
+        eventParameters.push("outdoor");
+    }
+    if ($("#sports-check").prop( "checked" )){
+        eventParameters.push("sports");
+    }
+    if ($("#music-check").prop( "checked" )){
+        eventParameters.push("music");
+    }
+    if ($("#nightlife-check").prop( "checked" )){
+        eventParameters.push("nightlife");
+    }
+    console.log(eventParameters);
     
     var dateTime = moment(startDate).format("YYYYMMDD")+"00-"+ moment(endDate).format("YYYYMMDD")+"00"
-    console.log(dateTime)
+    // console.log(dateTime)
     apiCall(locationz + "&within=25&page_number=1&page_size=6&sort_order=popularity&date=" + dateTime, "event")
 });
